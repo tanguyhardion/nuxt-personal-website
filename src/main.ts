53093baa4bf7f72589590553181cc0ddf4a1fe14
tanguyhardion <45904983+tanguyhardion@users.noreply.github.com>
2024-09-04 23:06:43 +0200

    npm deploy script

--- src/main.ts
+++ src/main.ts
@@ -9,12 +9,3 @@ const app = createApp(App);
 app.use(router);
 
 app.mount('#app');
-
-/*
-To deploy to gh-pages
-```
-npm run build
-git add dist && git commit -m "deploy to gh-pages"
-git subtree push --prefix dist origin gh-pages
-```
-*/
