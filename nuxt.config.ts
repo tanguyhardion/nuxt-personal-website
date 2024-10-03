97d83f2ebbd95409c86d9b09d46a888790ed6f55
tanguyhardion <45904983+tanguyhardion@users.noreply.github.com>
2024-10-03 23:08:23 +0200

    add nuxt-gtag

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,23 +7,17 @@ export default defineNuxtConfig({
   image: {
     dir: 'public/images'
   },
-  modules: ['@nuxt/image'],
+  modules: ['@nuxt/image', 'nuxt-gtag'],
+  gtag: {
+    enabled: process.env.NODE_ENV === 'production',
+    id: 'G-60YRWFVFZD'
+  },
   app: {
     head: {
       link: [
         { rel: 'icon', type: 'image/x-icon', href: 'https://i.imgur.com/ne4z3Jr.png' },
         { rel: 'stylesheet', href: 'https://fonts.googleapis.com/icon?family=Material+Icons' }
-      ],
-      script: [
-        { async: true, src: 'https://www.googletagmanager.com/gtag/js?id=G-60YRWFVFZD' },
-        {
-          innerHTML: `window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-          gtag('config', 'G-60YRWFVFZD');`
-        }
-      ],
-      title: 'Tanguy Hardion'
+      ]
     }
   }
 });
